4a8639ff76557a3721ad75787d66ba38df850635
Jonathan Cao <caojiasen@gmail.com>
2023-05-20 16:18:39 -0700

    more fixes for step 1

--- impls/rust.2/src/printer.rs
+++ impls/rust.2/src/printer.rs
@@ -1,45 +1,56 @@
-use std::{borrow::Cow, iter};
+use std::borrow::Cow;
 
 use itertools::Itertools;
 
-use crate::reader::{MalType, Pair};
+use crate::reader::MalType;
 
-pub fn pr_str(data: MalType<'_>) -> String {
+pub fn pr_str(data: MalType) -> String {
     data.to_string()
 }
 
-impl ToString for MalType<'_> {
+impl ToString for MalType {
     fn to_string(&self) -> String {
         match self {
-            MalType::Nil => "nil".into(),
+            MalType::Nil => "()".into(),
             MalType::Number(n) => n.to_string(),
-            MalType::Str(s) => s.to_string(),
+            MalType::String(s) => s.to_string(),
             MalType::Symbol(s) => s.to_string(),
-            MalType::List { pair, list } => iter::once(pair.left().into())
-                .chain(Itertools::intersperse(
-                    list.iter().map(|token| Cow::Owned(token.to_string())),
-                    " ".into(),
-                ))
-                .chain(iter::once(pair.right().into()))
-                .collect(),
+            MalType::List(l) => list_to_string(l.iter().map(|t| t.to_string().into()), "(", ")"),
+            MalType::Vector(v) => list_to_string(v.iter().map(|t| t.to_string().into()), "[", "]"),
+            MalType::Hashmap(h) => list_to_string(
+                h.iter().flat_map(|(k, v)| [k.into(), v.to_string().into()]),
+                "{",
+                "}",
+            ),
         }
     }
 }
 
-impl Pair {
-    fn left(self) -> &'static str {
-        match self {
-            Pair::Bracket => "[",
-            Pair::Brace => "{",
-            Pair::Parenthesis => "(",
-        }
-    }
-
-    fn right(self) -> &'static str {
-        match self {
-            Pair::Bracket => "]",
-            Pair::Brace => "}",
-            Pair::Parenthesis => ")",
-        }
-    }
+fn list_to_string<'a>(
+    iter: impl Iterator<Item = Cow<'a, str>>,
+    left: &'static str,
+    right: &'static str,
+) -> String {
+    std::iter::once(left.into())
+        .chain(Itertools::intersperse(iter, " ".into()))
+        .chain(std::iter::once(right.into()))
+        .collect()
 }
+
+// impl Pair {
+//     fn left(self) -> &'static str {
+//         match self {
+//             Pair::Bracket => "[",
+//             Pair::Brace => "{",
+//             Pair::Parenthesis => "(",
+//         }
+//     }
+
+//     fn right(self) -> &'static str {
+//         match self {
+//             Pair::Bracket => "]",
+//             Pair::Brace => "}",
+//             Pair::Parenthesis => ")",
+//         }
+//     }
+// }
--- impls/rust.2/src/reader.rs
+++ impls/rust.2/src/reader.rs
@@ -1,5 +1,6 @@
-use std::iter::Peekable;
+use std::{collections::HashMap, iter::Peekable};
 
+use itertools::Itertools;
 use once_cell::sync::OnceCell;
 use regex::Regex;
 use thiserror::Error;
@@ -35,8 +36,13 @@ fn to_token(s: &str) -> Option<ReadResult<Token<'_>>> {
         "(" => Left(Parenthesis),
         ")" => Right(Parenthesis),
         s if s.starts_with('"') => {
-            return Some(parse_string(s).map(Str).ok_or(ReadError::UnbalancedString))
+            return Some(
+                parse_string(s)
+                    .map(String)
+                    .ok_or(ReadError::UnbalancedString),
+            )
         }
+        s if s.starts_with(':') => String(s),
         s if s.starts_with(';') => return None,
         _ => Other(s),
     };
@@ -65,7 +71,7 @@ fn parse_string(s: &str) -> Option<&str> {
 pub enum Token<'a> {
     Left(Pair),
     Right(Pair),
-    Str(&'a str),
+    String(&'a str),
     Other(&'a str),
 }
 
@@ -82,6 +88,8 @@ pub enum ReadError {
     UnbalancedString,
     #[error("EOF")]
     Eof,
+    #[error("invalid hashmap")]
+    InvalidHashmap,
 }
 
 struct Reader<I>
@@ -101,18 +109,16 @@ where
         }
     }
 
-    fn read_form<'b>(&'b mut self) -> ReadResult<MalType<'a>> {
-        use Pair::*;
-        use Token::*;
-
+    fn read_form(&mut self) -> ReadResult<MalType> {
         let Some(token) = self.tokens.peek() else { return Ok(MalType::Nil) };
 
         match token {
-            Ok(Right(_)) => Err(ReadError::Eof),
-            &Ok(Left(pair)) => {
+            Ok(Token::Right(_)) => Err(ReadError::Eof),
+            &Ok(Token::Left(pair)) => {
                 self.tokens.next();
-                self.read_list(pair)
-                    .map(|list| MalType::List { pair, list })
+                let list = self.read_list(pair)?;
+
+                Self::parse_list(list, pair)
             }
             Ok(token) => {
                 if let Token::Other(other) = token {
@@ -120,10 +126,7 @@ where
                         self.tokens.next();
 
                         let tail = self.read_form()?;
-                        return Ok(MalType::List {
-                            pair: Parenthesis,
-                            list: vec![MalType::Symbol(symbol), tail],
-                        });
+                        return Ok(MalType::List(vec![MalType::Symbol(symbol.into()), tail]));
                     }
                 }
 
@@ -132,6 +135,34 @@ where
                 Ok(res)
             }
             &Err(e) => Err(e),
+        }
+    }
+
+    fn parse_list(list: Vec<MalType>, pair: Pair) -> ReadResult<MalType> {
+        match pair {
+            Pair::Brace => {
+                if list.len() % 2 != 0 {
+                    return Err(ReadError::InvalidHashmap);
+                }
+
+                list.into_iter()
+                    .tuples()
+                    .map(|(k, v)| {
+                        let MalType::String(k) = k else {
+                            return Err(ReadError::InvalidHashmap);
+                        };
+
+                        Ok((k, v))
+                    })
+                    .collect::<ReadResult<HashMap<_, _>>>()
+                    .map(MalType::Hashmap)
+            }
+            Pair::Bracket => Ok(MalType::Vector(list)),
+            Pair::Parenthesis => Ok(if list.is_empty() {
+                MalType::Nil
+            } else {
+                MalType::List(list)
+            }),
         }
     }
 
@@ -148,7 +179,7 @@ where
         Some(res)
     }
 
-    fn read_list<'b>(&'b mut self, pair: Pair) -> ReadResult<Vec<MalType<'a>>> {
+    fn read_list(&mut self, pair: Pair) -> ReadResult<Vec<MalType>> {
         use Token::*;
 
         let mut list = vec![];
@@ -171,25 +202,32 @@ where
         Err(ReadError::Eof)
     }
 
-    fn read_atom(token: &Token<'a>) -> MalType<'a> {
+    fn read_atom(token: &Token<'a>) -> MalType {
         match token {
-            Token::Str(s) => MalType::Str(s),
-            Token::Other(s) => s.parse().map(MalType::Number).unwrap_or(MalType::Symbol(s)),
+            Token::String(s) => MalType::String(s.to_string()),
+            Token::Other(s) => s
+                .parse()
+                .map(MalType::Number)
+                .unwrap_or(MalType::Symbol(s.to_string())),
             _ => unreachable!(),
         }
     }
 }
 
 #[derive(Debug)]
-pub enum MalType<'a> {
+pub enum MalType {
     Nil,
     Number(i64),
-    Str(&'a str),
-    Symbol(&'a str),
-    List { pair: Pair, list: Vec<MalType<'a>> },
+    String(String),
+    Symbol(String),
+    List(Vec<MalType>),
+    Vector(Vec<MalType>),
+    Hashmap(HashMap<String, MalType>),
 }
 
 #[test]
 fn feature() {
-    println!("{:#?}", read_str("'1"));
+    use crate::printer::pr_str;
+
+    println!("{}", pr_str(read_str("{\"abc\" 1}").unwrap()));
 }
